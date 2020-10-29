7b39dcefe5aa4bc447da422109b400f9c5102b24
resolritter <resolritter@users.noreply.github.com>
2020-10-29 15:00:00 -0300

    rename Home container to Books

--- web_ui/src/App.jsx
+++ web_ui/src/App.jsx
@@ -3,10 +3,10 @@ import { Route, Switch } from "react-router"
 
 import MainAppBar from "src/components/MainAppBar"
 import { routes } from "src/constants"
+import Books from "src/containers/Books"
 import CreateBook from "src/containers/CreateBook"
 import CreateUser from "src/containers/CreateUser"
 import FullPageLayout from "src/containers/FullPageLayout"
-import Home from "src/containers/Home"
 import Login from "src/containers/Login"
 
 export const ConnectedApp = function () {
@@ -14,7 +14,7 @@ export const ConnectedApp = function () {
     <FullPageLayout>
       <MainAppBar />
       <Switch>
-        <Route exact path={routes.home()} component={Home} />
+        <Route exact path={routes.home()} component={Books} />
         <Route exact path={routes.createBook()} component={CreateBook} />
         <Route exact path={routes.login()} component={Login} />
         <Route exact path={routes.createUser()} component={CreateUser} />
