ef71c1c7ddf80dbe1cfec83b2abafd822b63a3fe
resolritter <resolritter@users.noreply.github.com>
2020-10-29 15:00:00 -0300

    improve error handling pattern

--- web_ui/src/requests/index.js
+++ web_ui/src/requests/index.js
@@ -1,3 +1,4 @@
+import { getReasonPhrase } from "http-status-codes"
 import { apiURL, devAPIURL } from "src/constants"
 import { store } from "src/setup"
 
@@ -7,4 +8,12 @@ export const getCors = function () {
 
 export const getAccessToken = function () {
   return store.getState().user.accessToken
+}
+
+export const handleErrorResponse = async function (response) {
+  try {
+    return new Error((await response.json()).message)
+  } catch (err) {
+    return new Error(getReasonPhrase(response.status))
+  }
 }
